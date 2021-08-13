30c9e08c57522f7b5936bd1017d04a0f9f94a167
Bilwin <marf.zpd@gmail.com>
2021-08-13 18:04:08 +0900

    Initialize code

---
+++ code/Hud.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox.UI;
+
+//
+// You don't need to put things in a namespace, but it doesn't hurt.
+//
+namespace REngine
+{
+	/// <summary>
+	/// This is the HUD entity. It creates a RootPanel clientside, which can be accessed
+	/// via RootPanel on this entity, or Local.Hud.
+	/// </summary>
+	public partial class REngineHUD : Sandbox.HudEntity<RootPanel>
+	{
+		public REngineHUD()
+		{
+			if ( IsClient )
+			{
+				RootPanel.SetTemplate( "/Hud.html" );
+			}
+		}
+	}
+
+}
