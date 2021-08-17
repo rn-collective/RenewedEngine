ba807a7a79052d34668626d2a6a5faa31fcc82d3
Bilwin <marf.zpd@gmail.com>
2021-08-17 12:10:25 +0900

    Мелкие изменения

--- code/Game.cs
+++ code/Game.cs
@@ -5,12 +5,8 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
-//
-// You don't need to put things in a namespace, but it doesn't hurt.
-//
 namespace REngine
 {
-
 	/// <summary>
 	/// This is your game class. This is an entity that is created serverside when
 	/// the game starts, and is replicated to the client. 
@@ -18,29 +14,19 @@ namespace REngine
 	/// You can use this to create things like HUDs and declare which player class
 	/// to use for spawned players.
 	/// </summary>
-	[Library( "rengine", Title = "Renewed Engine" )]
+	[Library("rengine", Title = "Renewed Engine")]
+
 	public partial class REngine : Sandbox.Game
 	{
 		public REngine()
 		{
-			if ( IsServer )
+			if (IsServer)
 			{
-				Log.Info( "My Gamemode Has Created Serverside!" );
-
-				// Create a HUD entity. This entity is globally networked
-				// and when it is created clientside it creates the actual
-				// UI panels. You don't have to create your HUD via an entity,
-				// this just feels like a nice neat way to do it.
-				new REngineHUD();
-			}
-
-			if ( IsClient )
-			{
-				Log.Info( "My Gamemode Has Created Clientside!" );
+				_ = new REngineHUD();
 			}
 		}
 
-		/// <summary>
+	    /// <summary>
 		/// A client has joined the server. Make them a pawn to play with
 		/// </summary>
 		public override void ClientJoined(Client cl)
@@ -62,11 +48,13 @@ namespace REngine
 				Event.Run("PostPlayerNoclipped", cl);
 			}
 		}
+
 		public override void PostLevelLoaded()
 		{
 			base.PostLevelLoaded();
 			Event.Run("PostLevelLoaded");
 		}
+
 		public override void DoPlayerSuicide(Client cl) { }
 
 		[Event("OnClientInitialized")]
