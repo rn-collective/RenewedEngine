1b89be50f46ad06fc4a943563f2f4476a2a1b2fc
Bilwin <marf.zpd@gmail.com>
2021-08-17 12:10:42 +0900

    Update Player.cs

--- code/Player.cs
+++ code/Player.cs
@@ -10,25 +10,14 @@ namespace REngine
 		{
 			SetModel( "models/citizen/citizen.vmdl" );
 
-			//
-			// Use WalkController for movement (you can make your own PlayerController for 100% control)
-			//
 			Controller = new WalkController();
-
-			//
-			// Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
-			//
 			Animator = new StandardPlayerAnimator();
-
-			//
-			// Use ThirdPersonCamera (you can make your own Camera for 100% control)
-			//
 			Camera = new FirstPersonCamera();
 
 			EnableAllCollisions = true;
 			EnableDrawing = true;
 			EnableHideInFirstPerson = true;
-			EnableShadowInFirstPerson = true;
+			EnableShadowInFirstPerson = false;
 
 			base.Respawn();
 		}
@@ -40,17 +29,17 @@ namespace REngine
 		{
 			base.Simulate( cl );
 
-			//
-			// If you have active children (like a weapon etc) you should call this to 
-			// simulate those too.
-			//
 			SimulateActiveChild( cl, ActiveChild );
 		}
 
 		public override void OnKilled()
 		{
 			base.OnKilled();
+			Event.Run("OnPlayerKilled");
+		}
 
+		[Event("OnPlayerKilled")]
+		public void OnPlayerKilled() {
 			EnableDrawing = false;
 		}
 	}
